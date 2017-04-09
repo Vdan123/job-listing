d93a8a8849a5d892a15f771750f7027986468765
lsss <610550052@qq.com>
2017-04-10 07:49:31 +0800

    implement part4

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,8 +1,11 @@
-class Admin::JobsController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create, :destroy, :edit, :update]
-    before_action :admin_required
+class JobsController < ApplicationController
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+    def show
+        @job = Job.find(params[:id])
+      end
+
     def index
-        @jobs = Job.all
+        @jobs = Job.where(is_hidden: false).order(' created_at DESC')
     end
 
     def new
@@ -12,18 +15,9 @@ class Admin::JobsController < ApplicationController
     def create
         @job = Job.new(job_params)
         if @job.save
-            redirect_to admin_jobs_path
+            redirect_to jobs_path
         else
             render :new
-        end
-    end
-
-    def update
-        @job = Job.find(params[:id])
-        if @job.update(job_params)
-            redirect_to admin_jobs_path
-        else
-            render :edit
         end
     end
 
@@ -31,21 +25,19 @@ class Admin::JobsController < ApplicationController
         @job = Job.find(params[:id])
     end
 
-    def show
+    def update
         @job = Job.find(params[:id])
+        if @job.update(job_params)
+            redirect_to job_path(job)
+        else
+            render :edit
+        end
     end
 
     def destroy
         @job = Job.find(params[:id])
         @job.destroy
-        redirect_to admin_jobs_path
-    end
-
-    def admin_required
-        unless current_user.admin?
-            flash[:alert] = 'You are no admin'
-            redirect_to root_path
-        end
+        redirect_to job_path(job)
     end
 
     private
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,2 @@
 class Job < ApplicationRecord
-    validates :title, presence: true
-    validates :description, presence: true
-    validates :wage_lower_bound, presence: true
-    validates :wage_upper_bound, presence: true
-    validates :wage_lower_bound, numerically: { greater_than: 0 }
 end
