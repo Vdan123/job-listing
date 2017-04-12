90b6d2b3c6a7441c71a85be3f8eccdd4c4b029ad
lsss <610550052@qq.com>
2017-04-12 22:36:53 +0800

    add search gem ransack

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
     before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+    before_action :validate_search_key, only: [:search]
     def show
         @job = Job.find(params[:id])
 
@@ -52,9 +53,27 @@ class JobsController < ApplicationController
         redirect_to job_path(job)
     end
 
+    def search
+        if @query_string.present?
+            search_result = Job.ransack(@search_criteria).result(distinct: true)
+            @jobs = search_result.paginate(page: params[:page], per_page: 5)
+        end
+      end
+
     private
 
     def job_params
         params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
+    end
+
+    protected
+
+    def validate_search_key
+        @query_string = params[:q].gsub(/\\|\'|\/|\?/, '') if params[:q].present?
+        @search_criteria = search_criteria(@query_string)
+    end
+
+    def search_criteria(query_string)
+        { title_or_cotent_cont: query_string }
     end
 end
